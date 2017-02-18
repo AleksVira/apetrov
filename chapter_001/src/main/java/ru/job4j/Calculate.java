05eb2b616154aeed8dfd95ed0794ca0ddb8d4227
AleksVira <123all@newmail.ru>
2017-02-19 00:15:31 +0300

    Periods added

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,15 @@
 package ru.job4j;
 
-/**
-* Class Calculate, решение задачи части 001, урок 1
-* @author apetrov
-* @since 18.02.2017
-* @version 1
-*/
+/**.
+ * Class Calculate, решение задачи части 001, урок 1
+ * @author apetrov
+ * @since 18.02.2017
+ * @version 1
+ */
 
 public class Calculate {
 
-	/**
+	/**.
      * Main
      * @param args - args.
      */
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -6,7 +6,7 @@ import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-/**
+/**.
  * Test for Calculate
  *
  * @author apetrov
