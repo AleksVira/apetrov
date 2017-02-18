a7173285dfbc0ead405ddb9b89572ffee86475d9
AleksVira <123all@newmail.ru>
2017-02-18 15:40:39 +0300

    Code convention
    Added "package-info.java" and corrected "Calculate.java"

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+ * Class Calculate, решение задачи части 001, урок 1
+ * @author apetrov
+ * @since 18.02.2017
+ * @version 1
+ */
+
 public class Calculate {
+	
+	/**
+     * Main
+     * @param args - args.
+     */
 	public static void main(String[] args) {
 		System.out.println("Hello world!");	
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Aleksey Petrov (vira@ctsnet.ru)
+ * @since 18.02.2017
+ */
+package ru.job4j;
